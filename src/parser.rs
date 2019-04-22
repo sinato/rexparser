176e8c9c0ed051638352004c6222f034305f30ce
Shingo Kato <katomail0510@gmail.com>
2019-04-23 08:59:48 +0900

    Remove an associativity bug.

--- src/parser.rs
+++ src/parser.rs
@@ -21,32 +21,38 @@ fn get_precedence(op: &String) -> (u32, Associativity) {
 pub fn parse_entry(mut tokens: Tokens) -> Node {
     let lhs = Node::Token(TokenNode::from_consume(&mut tokens));
     if let Ok(_) = tokens.expect("Op") {
-        parse(lhs, &mut tokens)
+        parse(lhs, &mut tokens, 0)
     } else {
         lhs
     }
 }
 
-pub fn parse(mut lhs: Node, tokens: &mut Tokens) -> Node {
+pub fn parse(mut lhs: Node, tokens: &mut Tokens, min_precedence: u32) -> Node {
     while let Ok(Token::Op(op)) = tokens.expect("Op") {
         let (root_precedence, root_associativity) = get_precedence(&op);
+        if root_precedence < min_precedence {
+            break;
+        }
         let op = match tokens.consume_op() {
             Ok(op) => op,
             Err(msg) => panic!(msg),
         };
         let mut rhs = Node::Token(TokenNode::from_consume(tokens));
-        if let Ok(Token::Op(op2)) = tokens.expect("Op") {
+        while let Ok(Token::Op(op2)) = tokens.expect("Op") {
             let (precedence, _associativity) = get_precedence(&op2);
-
             match root_associativity {
                 Associativity::Right => {
                     if root_precedence <= precedence {
-                        rhs = parse(rhs, tokens);
+                        rhs = parse(rhs, tokens, precedence);
+                    } else {
+                        break;
                     }
                 }
                 Associativity::Left => {
                     if root_precedence < precedence {
-                        rhs = parse(rhs, tokens);
+                        rhs = parse(rhs, tokens, precedence);
+                    } else {
+                        break;
                     }
                 }
             }
@@ -146,12 +152,11 @@ mod tests {
     #[test]
     fn test_multi_terms2() {
         let actual = run(String::from("1 + 2 * 3 + 4 * 5"));
-        // expect: 1 + ((2 * 3) + (4 * 5))
-        let lhs = get_num(1);
-
-        let rhs1 = get_bin_exp("*", get_num(2), get_num(3));
-        let rhs2 = get_bin_exp("*", get_num(4), get_num(5));
-        let rhs = get_bin_exp("+", rhs1, rhs2);
+        // expect: (1 + (2 * 3)) + (4 * 5)
+        let lhs1 = get_num(1);
+        let lhs2 = get_bin_exp("*", get_num(2), get_num(3));
+        let lhs = get_bin_exp("+", lhs1, lhs2);
+        let rhs = get_bin_exp("*", get_num(4), get_num(5));
         let expected = get_bin_exp("+", lhs, rhs);
 
         assert_eq!(actual, expected);
@@ -160,7 +165,7 @@ mod tests {
     #[test]
     fn test_multi_terms_with_assign() {
         let actual = run(String::from("a = b = 1 + 2 * 3 + 4"));
-        // expect: a = (b = ((1 + (2 * 3)) + 4)) but... -> TODO
+        // expect: a = (b = ((1 + (2 * 3)) + 4))
         let lhs = get_ide("a");
         let rhs0 = get_bin_exp("*", get_num(2), get_num(3));
         let rhs1 = get_bin_exp("+", get_num(1), rhs0);
